58922caa87375f4bfd1691523f273263d5b92142
Danchenko Artemii <artemd-97@bk.ru>
2023-05-11 02:36:08 +0300

    change imports

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import pickle
 from catboost import CatBoostRegressor, Pool
 from hyperopt import fmin, space_eval, STATUS_OK
 
-from params import search_space, fit_params, fmin_args
+from params import fit_params, fmin_args
 from metrics import metrics_compute
 
 # TODO. Metrics, read-prepare data, utils, operator
@@ -30,7 +30,8 @@ def create_pools(df_train: pd.DataFrame,
 def train_model(df_train: pd.DataFrame, 
                 df_test: pd.DataFrame, 
                 target: str,
-                cat_features: list[str]
+                cat_features: list[str],
+                search_space: dict
                 ) -> dict:
 
     train_pool, eval_pool = create_pools(df_train.drop(target, axis=1),
@@ -45,18 +46,21 @@ def train_model(df_train: pd.DataFrame,
 
             model = CatBoostRegressor(**search_space)      
             model.fit(X=train_pool, eval_set=eval_pool, **fit_params)             
-            metrics = metrics_compute(df_test[target], call_predictions(model, eval_pool))
+            metrics = metrics_compute(df_test[target], 
+                                      call_predictions(model, eval_pool))
             
             # logging
             with open('catboost_model.pkl', 'wb') as f_out:
                 pickle.dump(model, f_out)
-            mlflow.log_artifact(local_path= "catboost_model.pkl", artifact_path= "model_pickle")
+            mlflow.log_artifact(local_path= "catboost_model.pkl", 
+                                artifact_path= "model_pickle")
             mlflow.log_metrics(metrics)
             mlflow.log_params({**search_space})
             mlflow.set_tag("model", type(model).__name__)
             mlflow.catboost.log_model(model, artifact_path= "models_mlflow") 
 
-            return {'loss': model.get_best_score()['validation']['RMSE'], 'status': STATUS_OK}
+            return {'loss': model.get_best_score()['validation']['RMSE'], 
+                    'status': STATUS_OK}
 
     best_params = fmin(fn = objective, **fmin_args)    
     hyperparams = space_eval(search_space, best_params)
