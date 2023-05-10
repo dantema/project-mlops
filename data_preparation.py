cdad31a3395995ddd690d492c91570385ef5df5e
Danchenko Artemii <artemd-97@bk.ru>
2023-05-11 00:37:28 +0300

    reading data and split

--- data_preparation.py
+++ data_preparation.py
@@ -2,11 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def read_prepare_data(path: str) -> tuple[pd.DataFrame, pd.DataFrame]:
-
     df = pd.read_csv(path)
-
-    df.loc[df["model"] == "Focus", "model"] = " Focus"
     df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
-
     return df_train, df_test
 
